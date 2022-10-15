4fd16d56233a06309c0006dbc3bfea3e1ecd4aa8
Molly B <103689113+mbruns13@users.noreply.github.com>
2022-10-15 14:24:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,15 +11,15 @@ from sklearn.metrics.pairwise import cosine_similarity
 #import spotify keys
 from config import client_id
 from config import client_secret
-from recommend_tracks import createToken, searchTrack, recommendSongs
+from recommend_tracks import recommendSongs
 
 regions = ['Argentina', 'Australia', 'Austria', 'Belarus', 'Belgium', 'Bolivia', 'Brazil', 'Bulgaria', 'Canada', 'Chile', 'Colombia', 'Costa Rica', 'Cyprus', 'Czech Republic', 'Denmark', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Estonia', 'Finland', 'France', 'Germany', 'Global', 'Greece', 'Guatemala', 'Honduras', 'Hong Kong', 'Hungary', 'Indonesia', 'Ireland', 'Israel', 'Italy', 'Japan', 'Kazakhstan', 'Latvia', 'Lithuania', 'Luxembourg', 'Malaysia', 'Mexico', 'Netherlands', 'New Zealand', 'Nicaragua', 'Nigeria', 'Norway', 'Pakistan', 'Panama', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Romania', 'Saudi Arabia', 'Singapore', 'Slovakia', 'South Africa', 'South Korea', 'Spain', 'Sweden', 'Switzerland', 'Taiwan', 'Thailand', 'Turkey', 'UAE', 'Ukraine', 'United Kingdom', 'Uruguay', 'Venezuela', 'Vietnam']
 
 app_mode = st.sidebar.selectbox('Select Page',['Rage Against The Machine Learning','Music Diversifier'])
 
-if app_mode=='Home':
-    st.title('Home')
-    st.markdown('Dataset:')
+if app_mode=='Rage Against The Machine Learning':
+    st.title('Rage Against The Machine Learning')
+    st.markdown('## Welcome!')
     # df=pd.read_csv('emp_analytics.csv')
     # st.write(df.head())
     st.write('testing')
@@ -27,19 +27,19 @@ if app_mode=='Home':
 
 if app_mode=='Music Diversifier':
     st.title('Music Diversifier')
-    st.markdown('Please type a song from Spotify :')
+    st.markdown('Please enter information about a song you like:')
     
     with st.form(key='my_form'):
-        track = st.text_input('Song name')
-        artist = st.text_input('Artist name')
-        st.markdown('Please select a region :')
-        region = st.selectbox('Pick One', regions)
+        track = st.text_input('Song name:')
+        artist = st.text_input('Artist name:')
+        # st.markdown('Regional Top 200 Chart:')
+        region = st.selectbox('Regional Top 200 Chart:', regions)
     
         clicked = st.form_submit_button("Find Songs")
  
         if clicked:
             results = recommendSongs(track, artist, region)
-            st.dataframe(results.iloc[:,1:4])
+            st.write(results.iloc[:,1:4])
 
 # if text_input('Song name')
     
