0df2d2bbaed3250b9c42bc1485e47d27b54e9b6a
Molly B <103689113+mbruns13@users.noreply.github.com>
2022-10-15 17:40:19 -0500

    adding images for team page

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 import streamlit.components.v1 as html
+from  PIL import Image
 from pprint import pprint
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, Normalizer
 from sklearn.metrics.pairwise import cosine_similarity
+
 
 #import spotify keys
 from config import client_id
@@ -18,8 +20,8 @@ regions = ['Argentina', 'Australia', 'Austria', 'Belarus', 'Belgium', 'Bolivia',
 
 
 with st.sidebar:
-    choose = option_menu("Select Page", ['Home','Music Diversifier'],
-                         icons=['house', 'music-player-fill'],
+    choose = option_menu(None, ['Home','Music Diversifier', 'Our Team'],
+                         icons=['house', 'music-player-fill', 'people-fill'],
                          menu_icon="app-indicator", default_index=0,
                          styles={
         "container": {"padding": "5!important", "background-color": "#white"},
@@ -34,8 +36,6 @@ with st.sidebar:
 if choose=='Home':
     st.title('Rage Against The Machine Learning')
     st.markdown('## Welcome to our app!')
-    # df=pd.read_csv('emp_analytics.csv')
-    # st.write(df.head())
     st.write("We've created a Music Diversifier to help expand your musical horizons. Once you input a favorite song of yours and select a region you're interested in, our app will return the five most similar songs from that region's Top 200 Chart.")
     st.write("Please note: our app is currently only working with chart data from the week of 9/30/22 - 10/6/22.")
 
@@ -51,12 +51,12 @@ if choose=='Music Diversifier':
         region = st.selectbox('Regional Top 200 Chart:', regions)
     
         clicked = st.form_submit_button("Find Songs")
-        
-        st.markdown('---')
+    
 
         if clicked:
             results = recommendSongs(track, artist, region)
 
+            st.markdown(""" ### Results: """)
             st.write(results.iloc[:,1:4])
 
             results_info = results.drop(columns=['Similarity Score'])
@@ -78,4 +78,47 @@ if choose=='Music Diversifier':
 
             combined_df = pd.concat([input_info,results_info.loc[:]]).reset_index(drop=True)    
             st.write(combined_df.iloc[:,1:13])
+
+
+# github_logo = Image.open('assets/img/team/GitHub-Mark-120px-plus.png')
+# linkedin_logo = Image.open('assets/img/team/LI-In-Bug.png')
+dan_profile = Image.open('assets/img/team/dan.jpeg')
+kevin_profile = Image.open('assets/img/team/kevin.jpeg')
+molly_profile = Image.open('assets/img/team/molly.jpeg')
+slone_profile = Image.open('assets/img/team/slone.jpeg')
+
+    
+if choose=='Our Team':
+    st.title('Our Team')
+    col1, col2 = st.columns( [0.5, 0.5])
+
+    with col1: 
+        st.markdown('## Dan')
+        st.image(dan_profile, width=150 )
+        st.markdown("insert blurb here")
+        st.markdown("""[GitHub](https://github.com/dtmurphy6)    
+        [LinkedIn](https://www.linkedin.com/in/danielmurphy3/)""")
+
+        st.markdown('## Molly')
+        st.image(molly_profile, width=150 )
+        st.markdown("insert blurb here")
+        st.markdown("""[GitHub](https://github.com/mbruns13)    
+        [LinkedIn](https://www.linkedin.com/in/mollybruns)""")
+
+
+    with col2: 
+        st.markdown('## Kevin')
+        st.image(kevin_profile, width=150 )
+        st.markdown("insert blurb here")
+    # st.markdown("""<a href="https://github.com/flores-kevin"><i class="bi bi-github"></i></a>
+        #                                         <a href="https://www.linkedin.com/in/kevin-flores-097991237/"><i class="bi bi-linkedin"></i></a>
+        # """)
+        st.markdown("""[GitHub](https://github.com/flores-kevin)    
+        [LinkedIn](https://www.linkedin.com/in/kevin-flores-097991237)""")
+
+        st.markdown('## Slone')
+        st.image(slone_profile, width=150 )
+        st.markdown("insert blurb here")
+        st.markdown("""[GitHub](https://github.com/bslone1)    
+        [LinkedIn](https://www.linkedin.com/in/bridget-slone-021bs/)""")
 
